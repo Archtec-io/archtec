20949ce308b3ec09a999e17a9d7992aea935d7b1
debagos <debagos@users.noreply.github.com>
2022-06-15 20:32:46 +0200

    Unify log message and colorize output.

--- scripts/prejoin.lua
+++ scripts/prejoin.lua
@@ -1,5 +1,8 @@
+local modName = minetest.get_current_modname()
+
 minetest.register_on_prejoinplayer(function(name, ip)
-	minetest.log("action", "Player " .. name .. " prejoins from " .. ip)
-	logMessage = ("Player: "..name.." prejoins")
-	notifyTeam("[Server] " .. name .. " prejoins from " .. ip)
+	local logMessage = "["..modName.."] Connection initialisation by '"..name.."' (IP: "..ip..")"
+	minetest.log("action", logMessage)
+	notifyTeam(minetest.colorize("#666", logMessage))
+	-- It's probably a better idea to just colorize everything inside the notifyTeam function.
 end)
