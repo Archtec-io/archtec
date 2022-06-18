33d45fd7a04a2d418d30bb38dd4581813dbb377c
Niklp09 <ngs.nik@outlook.de>
2022-06-18 14:52:40 +0200

    redo shutdown.lua

--- scripts/shutdown.lua
+++ scripts/shutdown.lua
@@ -1,6 +1,8 @@
 local function requestShutdown(playerName, params)
-    minetest.chat_send_all(minetest.colorize("#FF0000", (playerName.." möchte den Server in 10 Sekunden Herunterfahren")))
-    minetest.sound_play("teleport.ogg")
+    minetest.chat_send_all(minetest.colorize("#FF0000", (playerName.." requested a server shutdown in 10 seconds.")))
+    if minetest.get_modpath("chatplus_discord") then
+        discord.send(':anger: '..playerName..' requested a server shutdown in 10 seconds.')
+    end
     minetest.after(10, function()
         minetest.request_shutdown("Please click 'Reconect' in 1 minute.",true,0)
     end)
