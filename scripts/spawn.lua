fee6768b77150e95c778c121b01fc5b77f360dd0
Niklp09 <ngs.nik@outlook.de>
2022-06-18 17:53:18 +0200

    add /s command

--- scripts/spawn.lua
+++ scripts/spawn.lua
@@ -11,3 +11,13 @@ minetest.register_chatcommand("spawn", {
 		end
 	end
 })
+
+minetest.register_chatcommand("s", {
+	privs = { spawn = true },
+	func = function(name, param)
+		local player = minetest.get_player_by_name(name)
+		if player ~= nil then
+			player:set_pos({ x =-194, y = 10, z = 482 })
+		end
+	end
+})
