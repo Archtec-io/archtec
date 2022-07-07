604eeb913ce43e03ccc18d6e8f99473e1a60f86e
debagos <juri.ossiek@gmx.de>
2022-07-07 21:37:55 +0200

    Improved the Discord '/report' feature.

--- archtec/scripts/report_webhook.lua
+++ archtec/scripts/report_webhook.lua
@@ -1,29 +1,43 @@
 local http = assert(...)
 local webhook_url = minetest.settings:get("archtec.webhook_url")
 
-local function send_webhook(player_name, report)
-    local data = minetest.write_json({
-        embeds = {
-            {
-                title = "Report from " .. player_name,
-                description = report
-            }
-        }
+local function send_report(name, report)
+    local json = minetest.write_json({
+		embeds = {{
+			title = "Report by "..name..":",
+			description = report
+		}}
     })
-    http.fetch({
-        url = webhook_url,
-        method = "POST",
-        extra_headers = {"Content-Type: application/json"},
-        data = data,
-        post_data = data
-    }, function() end)
+    if json == nil then
+		minetest.log("warning", "Failed to create JSON for '/report'. Report parameters: "..dump(report))
+		return false
+	else
+		http.fetch({
+			url = webhook_url,
+			method = "POST",
+			extra_headers = { "Content-Type: Application/JSON" },
+			data = json,
+		}, function() end)
+		return true
+	end
 end
 
-minetest.register_chatcommand("report", {
-    privs = {interact=true},
-    func = function(name, param)
-        send_webhook(name, param)
-        minetest.chat_send_player(name, "Report created successfully")
-        return
-    end,
-})
+if webhook_url == nil then
+	minetest.log("warning", "No Discord webhook URL found in the settings. Please specify 'archtec.webhook_url' in the settings. The '/report' command is not available now.")
+else
+	minetest.register_chatcommand("report", {
+		privs = { interact = true },
+		func = function(name, params)
+			local param = params:trim()
+			if param ~= "" then
+				if send_report(name, param) then
+					return true, "Report successfully created."
+				else
+					return false, "Error: Creating report failed. You are using illegal characters and/or symbols."
+				end
+			else
+				return false, "Error: You have to provide a report text."
+			end
+		end,
+	})
+end
