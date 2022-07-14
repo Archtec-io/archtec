fde2164f15f3d98a83298af7e920c230e048a928
Niklp <89982526+Niklp09@users.noreply.github.com>
2022-07-14 19:14:30 +0200

    fix typo

--- archtec/scripts/mvps_stopper.lua
+++ archtec/scripts/mvps_stopper.lua
@@ -1,4 +1,4 @@
-if minetest.get_modpath("(3d_armor_stand") then
+if minetest.get_modpath("3d_armor_stand") then
     mesecon.register_mvps_stopper("3d_armor_stand:armor_stand")
     mesecon.register_mvps_stopper("3d_armor_stand:locked_armor_stand")
 end
