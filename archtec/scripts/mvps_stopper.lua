f97451d972e8dabf55d58210ae1efbdc2156ebc9
debagos <juri.ossiek@gmx.de>
2022-07-15 07:30:33 +0200

    Making sure 'mesecons_mvps' and '3d_armor_stand' mods are loaded before we try to do something with it, which prevents possible nil crash.

--- archtec/scripts/mvps_stopper.lua
+++ archtec/scripts/mvps_stopper.lua
@@ -1,4 +1,4 @@
-if minetest.get_modpath("3d_armor_stand") then
-    mesecon.register_mvps_stopper("3d_armor_stand:armor_stand")
-    mesecon.register_mvps_stopper("3d_armor_stand:locked_armor_stand")
+if minetest.get_modpath("mesecons_mvps") ~= nil and minetest.get_modpath("3d_armor_stand") ~= nil then
+	mesecon.register_mvps_stopper("3d_armor_stand:armor_stand")
+	mesecon.register_mvps_stopper("3d_armor_stand:locked_armor_stand")
 end
