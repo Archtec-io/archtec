02c982c631a7d76b9e8f3275cdc1e66b5bdca071
Niklp09 <ngs.nik@outlook.de>
2022-08-18 20:40:46 +0200

    remove old unused code

--- archtec/scripts/unregister.lua
+++ archtec/scripts/unregister.lua
@@ -3,11 +3,6 @@ if minetest.get_modpath("atm") then
     minetest.unregister_item("atm:atm2")
 end
 
-if minetest.get_modpath("tpr") then
-    minetest.unregister_chatcommand("tpc")
-    minetest.unregister_chatcommand("tpj")
-end
-
 minetest.unregister_chatcommand("ban")
 minetest.unregister_chatcommand("unban")
 
