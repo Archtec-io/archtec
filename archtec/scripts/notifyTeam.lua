3b69cad11f7c8bcae937a15310d9730904d9db0d
Niklp09 <89982526+Niklp09@users.noreply.github.com>
2022-09-17 12:53:43 +0200

    fix crash in notifyTeam function when 'chatplus_discord' isn't available

--- archtec/scripts/notifyTeam.lua
+++ archtec/scripts/notifyTeam.lua
@@ -1,12 +1,26 @@
-function notifyTeam(message)
-    for _, player in ipairs(minetest.get_connected_players()) do
-        local name = player:get_player_name()
-        if name then
-            local hasPrivs, missingPrivs = minetest.check_player_privs(name, "server")
-            if hasPrivs then
-                minetest.chat_send_player(name, message)
+if minetest.get_modpath("chatplus_discord") then
+    function notifyTeam(message)
+        for _, player in ipairs(minetest.get_connected_players()) do
+            local name = player:get_player_name()
+            if name then
+                local hasPrivs, missingPrivs = minetest.check_player_privs(name, "server")
+                if hasPrivs then
+                    minetest.chat_send_player(name, message)
+                end
+            end
+        end
+        discord.send(message, "1002864057398870066")
+    end
+else
+    function notifyTeam(message)
+        for _, player in ipairs(minetest.get_connected_players()) do
+            local name = player:get_player_name()
+            if name then
+                local hasPrivs, missingPrivs = minetest.check_player_privs(name, "server")
+                if hasPrivs then
+                    minetest.chat_send_player(name, message)
+                end
             end
         end
     end
-    discord.send(message, "1002864057398870066")
 end
