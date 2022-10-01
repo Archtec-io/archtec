5a14f952248c0cb067e63bea1bd8b13eb7ad93d1
Niklp09 <89982526+Niklp09@users.noreply.github.com>
2022-10-01 21:56:33 +0200

    crafting compat 'homedecor:table_legs_wood/ropes:ladder_wood'

--- archtec/scripts/crafting.lua
+++ archtec/scripts/crafting.lua
@@ -1,13 +1,28 @@
 if minetest.get_modpath("pride_flags") then
     minetest.clear_craft({
-        output="pride_flags:lower_mast"
+        output = "pride_flags:lower_mast"
     })
     minetest.register_craft({
-        output="pride_flags:lower_mast",
-        recipe={
+        output = "pride_flags:lower_mast",
+        recipe = {
             {"default:steel_ingot", "farming:string"},
             {"default:steel_ingot", "farming:string"},
             {"default:steel_ingot", "farming:string"},
+        }
+    })
+end
+
+--homedecor:table_legs_wood/ropes:ladder_wood
+if minetest.get_modpath("ropes") then
+    minetest.clear_craft({
+        output = "ropes:ladder_wood"
+    })
+    minetest.register_craft({
+        output = "ropes:ladder_wood",
+        recipe = {
+            {"group:stick", "", "group:stick"},
+            {"group:stick", "", "group:stick"},
+            {"group:stick", "group:stick", "group:stick"},
         }
     })
 end
