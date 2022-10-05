7d21624c68fb48d2795e3e8267f1484be31908d0
Niklp09 <89982526+Niklp09@users.noreply.github.com>
2022-10-05 16:11:52 +0200

    grant the forceload priv automatically

--- archtec/scripts/techage.lua
+++ archtec/scripts/techage.lua
@@ -25,3 +25,22 @@ minetest.override_item("techage:forceloadtile", {
         end
     end
 })
+
+local old_on_place = minetest.registered_nodes["techage:ta3_drillbox_pas"].on_place or function() end
+minetest.override_item("techage:ta3_drillbox_pas", {
+    on_place = function(itemstack, placer, pointed_thing)
+        local pname = placer:get_player_name()
+
+        if not minetest.check_player_privs(pname, "forceload") then
+            local privs = minetest.get_player_privs(pname)
+            privs["forceload"] = true
+            minetest.set_player_privs(pname, privs)
+            minetest.chat_send_player(pname, "Congratulations! You have been granted the 'forceload' privilege")
+            local logMessage = "[techage] Granted '"..pname.."' the 'forceload' priv"
+            notifyTeam(minetest.colorize("#666", logMessage))
+            return old_on_place(itemstack, placer, pointed_thing)
+        else
+            return old_on_place(itemstack, placer, pointed_thing)
+        end
+    end
+})
