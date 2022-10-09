39f1806d95883a3ee1daf4ab4866af6991d7ca9d
Niklp09 <89982526+Niklp09@users.noreply.github.com>
2022-10-09 14:35:14 +0200

    /sd: add support for custom delay times

--- archtec/scripts/shutdown.lua
+++ archtec/scripts/shutdown.lua
@@ -1,13 +1,12 @@
 minetest.register_chatcommand("sd", {
 	description = "Shuts the server down after a 10 seconds delay.",
 	privs = { server = true },
-	func = function(playerName, params)
-		local logStr = playerName.." requested a server shutdown in 10 seconds."
+	func = function(playerName, delay)
+		if delay == '' then delay = 10 end
+		local logStr = playerName .. " requested a server shutdown in " .. delay .. " seconds."
 		minetest.log("warning", logStr)
 		minetest.chat_send_all(minetest.colorize("#FF0", logStr))
-		if minetest.get_modpath("chatplus_discord") then
-			discord.send(":anger: "..logStr)
-		end
-		minetest.request_shutdown("The server is rebooting, please reconnect in about a minute.", true, 10)
+		discord.send(":anger: "..logStr)
+		minetest.request_shutdown("The server is rebooting, please reconnect in about a minute.", true, delay)
 	end
 })
