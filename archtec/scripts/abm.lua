abd61784f04a459974c5dd2a39a207ef45dfa7ee
Niklp09 <89982526+Niklp09@users.noreply.github.com>
2022-10-25 21:06:50 +0200

    override abms correctly

--- archtec/scripts/abm.lua
+++ archtec/scripts/abm.lua
@@ -1,4 +1,4 @@
---override bees abm
+--override abms
 for _, ab in pairs(minetest.registered_abms) do
 
 	local label = ab.label or ""
@@ -7,44 +7,38 @@ for _, ab in pairs(minetest.registered_abms) do
     if label == "spawn bee hives" and node1 == "group:leaves" then
 		ab.nodenames = {"default:leaves"}
     end
-end
 
---override spawn chances of mobds
-for _, ab in pairs(minetest.registered_abms) do
-
-	local label = ab.label or ""
-
-    if label == "mobs_animal:bunny" then
+	if label == "mobs_animal:bunny spawning" then
 		ab.chance = 6000 --8000
-	elseif label == "mobs_animal:chicken" then
+	elseif label == "mobs_animal:chicken spawning" then
 		ab.chance = 6000 --8000
-	elseif label == "mobs_animal:cow" then
+	elseif label == "mobs_animal:cow spawning" then
 		ab.chance = 6000 --8000
-	elseif label == "mobs_animal:kitten" then
+	elseif label == "mobs_animal:kitten spawning" then
 		ab.chance = 8000 --10000
-	elseif label == "mobs_animal:panda" then
+	elseif label == "mobs_animal:panda spawning" then
 		ab.chance = 6000 --8000
-	elseif label == "mobs_animal:sheep_white" then
+	elseif label == "mobs_animal:sheep_white spawning" then
 		ab.chance = 6000 --8000
-	elseif label == "mobs_animal:pumba" then
+	elseif label == "mobs_animal:pumba spawning" then
 		ab.chance = 6000 --8000
 	end
 
-	if label == "mobs_monster:dirt_monster" then
+	if label == "mobs_monster:dirt_monster spawning" then
 		ab.chance = 3500 --6000
-	elseif label == "mobs_monster:dungeon_master" then
+	elseif label == "mobs_monster:dungeon_master spawning" then
 		ab.chance = 7000 --9000
-	elseif label == "mobs_monster:mese_monster" then
+	elseif label == "mobs_monster:mese_monster spawning" then
 		ab.chance = 3500 --5000
-	elseif label == "mobs_monster:oerkki" then
+	elseif label == "mobs_monster:oerkki spawning" then
 		ab.chance = 3500 --7000
-	elseif label == "mobs_monster:sand_monster" then
+	elseif label == "mobs_monster:sand_monster spawning" then
 		ab.chance = 3500 --5000
-	elseif label == "mobs_monster:spider" then
+	elseif label == "mobs_monster:spider spawning" then
 		ab.chance = 5000 --7000
-	elseif label == "mobs_monster:stone_monster" then
+	elseif label == "mobs_monster:stone_monster spawning" then
 		ab.chance = 3500 --7000
-	elseif label == "mobs_monster:tree_monster" then
+	elseif label == "mobs_monster:tree_monster spawning" then
 		ab.chance = 3500 --7000
 	end
 end
