7cb96cf673f86a70472d8b345de7988f879cadc2
Niklp09 <89982526+Niklp09@users.noreply.github.com>
2022-10-25 20:32:45 +0200

    modify spawn abms directly

--- archtec/scripts/abm.lua
+++ archtec/scripts/abm.lua
@@ -8,3 +8,43 @@ for _, ab in pairs(minetest.registered_abms) do
 		ab.nodenames = {"default:leaves"}
     end
 end
+
+--override spawn chances of mobds
+for _, ab in pairs(minetest.registered_abms) do
+
+	local label = ab.label or ""
+
+    if label == "mobs_animal:bunny" then
+		ab.chance = 6000 --8000
+	elseif label == "mobs_animal:chicken" then
+		ab.chance = 6000 --8000
+	elseif label == "mobs_animal:cow" then
+		ab.chance = 6000 --8000
+	elseif label == "mobs_animal:kitten" then
+		ab.chance = 8000 --10000
+	elseif label == "mobs_animal:panda" then
+		ab.chance = 6000 --8000
+	elseif label == "mobs_animal:sheep_white" then
+		ab.chance = 6000 --8000
+	elseif label == "mobs_animal:pumba" then
+		ab.chance = 6000 --8000
+	end
+
+	if label == "mobs_monster:dirt_monster" then
+		ab.chance = 3500 --6000
+	elseif label == "mobs_monster:dungeon_master" then
+		ab.chance = 7000 --9000
+	elseif label == "mobs_monster:mese_monster" then
+		ab.chance = 3500 --5000
+	elseif label == "mobs_monster:oerkki" then
+		ab.chance = 3500 --7000
+	elseif label == "mobs_monster:sand_monster" then
+		ab.chance = 3500 --5000
+	elseif label == "mobs_monster:spider" then
+		ab.chance = 5000 --7000
+	elseif label == "mobs_monster:stone_monster" then
+		ab.chance = 3500 --7000
+	elseif label == "mobs_monster:tree_monster" then
+		ab.chance = 3500 --7000
+	end
+end
