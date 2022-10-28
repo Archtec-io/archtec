65f3a2ab17ea51a863443a396f01f04d0838cacd
Niklp09 <89982526+Niklp09@users.noreply.github.com>
2022-10-28 15:20:30 +0200

    solve mob spawn groups manual
    performance:) @debagos

--- archtec/scripts/abm.lua
+++ archtec/scripts/abm.lua
@@ -1,8 +1,38 @@
+local group_grass = {
+	"default:dry_grass_1", 
+	"default:dry_grass_2",
+	"default:dry_grass_3",
+	"default:dry_grass_4",
+	"default:dry_grass_5",
+	"default:fern_1",
+	"default:fern_2",
+	"default:fern_3",
+	"default:grass_1",
+	"default:grass_2",
+	"default:grass_3",
+	"default:grass_4",
+	"default:grass_5",
+	"default:junglegrass",
+	"default:marram_grass_1",
+	"default:marram_grass_2",
+	"default:marram_grass_3",
+}
+
+local group_dry_grass = {
+	"default:dry_grass_1",
+	"default:dry_grass_2",
+	"default:dry_grass_3",
+	"default:dry_grass_4",
+	"default:dry_grass_5",
+}
+
 --override abms
 for _, ab in pairs(minetest.registered_abms) do
 
 	local label = ab.label or ""
 	local node1 = ab.nodenames and ab.nodenames[1] or ""
+	local node2 = ab.nodenames and ab.nodenames[2] or ""
+	local neigh = ab.neighbors and ab.neighbors[1] or ""
 
     if label == "spawn bee hives" and node1 == "group:leaves" then
 		ab.nodenames = {"default:leaves"}
@@ -10,18 +40,25 @@ for _, ab in pairs(minetest.registered_abms) do
 
 	if label == "mobs_animal:bunny spawning" then
 		ab.chance = 6000 --8000
+		ab.neighbours = group_grass
 	elseif label == "mobs_animal:chicken spawning" then
 		ab.chance = 6000 --8000
+		ab.neighbours = group_grass
 	elseif label == "mobs_animal:cow spawning" then
 		ab.chance = 6000 --8000
+		ab.neighbours = group_grass
 	elseif label == "mobs_animal:kitten spawning" then
 		ab.chance = 8000 --10000
+		ab.neighbours = group_grass
 	elseif label == "mobs_animal:panda spawning" then
 		ab.chance = 6000 --8000
+		ab.neighbours = group_grass
 	elseif label == "mobs_animal:sheep_white spawning" then
 		ab.chance = 6000 --8000
+		ab.neighbours = group_grass
 	elseif label == "mobs_animal:pumba spawning" then
 		ab.chance = 6000 --8000
+		ab.neighbours = group_dry_grass
 	end
 
 	if label == "mobs_monster:dirt_monster spawning" then
