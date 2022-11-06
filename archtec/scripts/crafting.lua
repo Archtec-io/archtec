32dc906755bffe162e5bd538052d84970d289adb
Niklp09 <89982526+Niklp09@users.noreply.github.com>
2022-11-06 14:05:41 +0100

    remove ropes recipe override (fixed upstream)

--- archtec/scripts/crafting.lua
+++ archtec/scripts/crafting.lua
@@ -12,21 +12,6 @@ if minetest.get_modpath("pride_flags") then
     })
 end
 
---homedecor:table_legs_wood/ropes:ladder_wood
-if minetest.get_modpath("ropes") then
-    minetest.clear_craft({
-        output = "ropes:ladder_wood"
-    })
-    minetest.register_craft({
-        output = "ropes:ladder_wood",
-        recipe = {
-            {"group:stick", "", "group:stick"},
-            {"group:stick", "", "group:stick"},
-            {"group:stick", "group:stick", "group:stick"}
-        }
-    })
-end
-
 --homedecor:table/ts_furniture:default_WOODTYPE_small_table
 if minetest.get_modpath("homedecor_tables") then
     minetest.clear_craft({
@@ -92,6 +77,7 @@ if minetest.get_modpath("ethereal") then
     })
 end
 
+--jonez palace windows have the same recipe
 if minetest.get_modpath("jonez") then
     minetest.clear_craft({
         output = "xpanes:palace_window_top_flat"
