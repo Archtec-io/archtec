7a6ed8667c26f1b3ab387a765401314e3d58c18d
Niklp09 <89982526+Niklp09@users.noreply.github.com>
2022-11-23 13:39:07 +0100

    code style

--- archtec/scripts/split_long_msg.lua
+++ archtec/scripts/split_long_msg.lua
@@ -2,7 +2,7 @@ local old_chat_send_player = minetest.chat_send_player
 
 function minetest.chat_send_player(name, text)
 	if #text > 65000 then
-        old_chat_send_player("Message blocked because it's to long!")
+    	old_chat_send_player("Message blocked because it's to long!")
 	end
 
 	if #text > 0 then
@@ -14,7 +14,7 @@ local old_chat_send_all = minetest.chat_send_all
 
 function minetest.chat_send_all(text)
 	if #text > 65000 then
-        old_chat_send_all("Message blocked because it's to long!")
+    	old_chat_send_all("Message blocked because it's to long!")
 	end
 
 	if #text > 0 then
