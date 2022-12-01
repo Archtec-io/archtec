e8368342b2208198d2b55b92e0e95e3cfdac02cc
Niklp <89982526+Niklp09@users.noreply.github.com>
2022-12-01 21:28:16 +0100

    fix https://github.com/Archtec-io/bugtracker/issues/25

--- archtec/scripts/split_long_msg.lua
+++ archtec/scripts/split_long_msg.lua
@@ -1,8 +1,21 @@
 local old_chat_send_player = minetest.chat_send_player
 
 function minetest.chat_send_player(name, text)
-	if #text > 65000 then
-		old_chat_send_player("Message blocked because it's to long!")
+	while #text > 65000 do
+		local found_newline = false
+		for i = 65000, 1, -1 do
+			if text:sub(i, i) == "\n" then
+				old_chat_send_player(name, text:sub(1, i - 1))
+				text = text:sub(i + 1)
+				found_newline = true
+				break
+			end
+		end
+
+		if not found_newline then
+			old_chat_send_player(name, text:sub(1, 65000))
+			text = text:sub(65000 + 1)
+		end
 	end
 
 	if #text > 0 then
@@ -13,8 +26,21 @@ end
 local old_chat_send_all = minetest.chat_send_all
 
 function minetest.chat_send_all(text)
-	if #text > 65000 then
-		old_chat_send_all("Message blocked because it's to long!")
+	while #text > 65000 do
+		local found_newline = false
+		for i = 65000, 1, -1 do
+			if text:sub(i, i) == "\n" then
+				old_chat_send_all(text:sub(1, i - 1))
+				text = text:sub(i + 1)
+				found_newline = true
+				break
+			end
+		end
+
+		if not found_newline then
+			old_chat_send_all(text:sub(1, 65000))
+			text = text:sub(65000 + 1)
+		end
 	end
 
 	if #text > 0 then
