a89382e991b60bc59d0924aabf66a97dc47eae1b
Niklp <89982526+Niklp09@users.noreply.github.com>
2022-12-10 12:23:15 +0100

    some stamina code improvements

--- stamina/init.lua
+++ stamina/init.lua
@@ -27,26 +27,18 @@ local function is_player(player)
 	)
 end
 
-local function set_player_attribute(player, key, value)
-	if player.get_meta then
-		local meta = player:get_meta()
-		if meta and value == nil then
-			meta:set_string(key, "")
-		elseif meta then
-			meta:set_string(key, tostring(value))
-		end
-	else
-		player:set_attribute(key, value)
+local function set_player_meta(player, key, value)
+	local meta = player:get_meta()
+	if meta and value == nil then
+		meta:set_string(key, "")
+	elseif meta then
+		meta:set_string(key, tostring(value))
 	end
 end
 
-local function get_player_attribute(player, key)
-	if player.get_meta then
-		local meta = player:get_meta()
-		return meta and meta:get_string(key) or ""
-	else
-		return player:get_attribute(key)
-	end
+local function get_player_meta(player, key)
+	local meta = player:get_meta()
+	return meta and meta:get_string(key) or ""
 end
 
 local hud_ids_by_player_name = {}
@@ -61,11 +53,11 @@ end
 
 --- SATURATION API ---
 function archtec_stamina.get_saturation(player)
-	return tonumber(get_player_attribute(player, attribute.saturation))
+	return tonumber(get_player_meta(player, attribute.saturation))
 end
 
 function archtec_stamina.set_saturation(player, level)
-	set_player_attribute(player, attribute.saturation, level)
+	set_player_meta(player, attribute.saturation, level)
 	player:hud_change(
 		get_hud_id(player),
 		"number",
@@ -105,11 +97,11 @@ archtec_stamina.exhaustion_reasons = {
 }
 
 function archtec_stamina.get_exhaustion(player)
-	return tonumber(get_player_attribute(player, attribute.exhaustion))
+	return tonumber(get_player_meta(player, attribute.exhaustion))
 end
 
 function archtec_stamina.set_exhaustion(player, exhaustion)
-	set_player_attribute(player, attribute.exhaustion, exhaustion)
+	set_player_meta(player, attribute.exhaustion, exhaustion)
 end
 
 function archtec_stamina.exhaust_player(player, change, cause)
@@ -188,11 +180,9 @@ function minetest.do_item_eat(hp_change, replace_with_item, itemstack, player, p
 		return itemstack
 	end
 
-	local player_pos = player:get_pos()
-
 	minetest.sound_play("hbhunger_eat_generic", {
 		object = player,
-		pos = player_pos,
+		pos = player:get_pos(),
 		max_hear_distance = 16,
 	}, true)
 
@@ -208,7 +198,7 @@ function minetest.do_item_eat(hp_change, replace_with_item, itemstack, player, p
 			itemstack:add_item(replace_with_item)
 		else
 			local inv = player:get_inventory()
-			if inv:room_for_item("main", {name = replace_with_item}) then
+			if inv and inv:room_for_item("main", {name = replace_with_item}) then
 				inv:add_item("main", replace_with_item)
 			else
 				local pos = player:get_pos()
