3dea723dff26a73599786d4fba26220746f60d3d
Niklp <89982526+Niklp09@users.noreply.github.com>
2022-12-15 10:47:16 +0100

    new spawn pos

--- archtec/scripts/spawn.lua
+++ archtec/scripts/spawn.lua
@@ -5,16 +5,33 @@ minetest.register_privilege("spawn", {
 local function movePlayerToSpawn(playerName)
 	local player = minetest.get_player_by_name(playerName)
 	if player ~= nil then
-		player:set_pos({ x =325, y = 12, z = 1120 })
+		player:set_pos({x = 236.5, y = 16, z = -2033.5})
+	end
+end
+
+local function movePlayerToOldSpawn(playerName)
+	local player = minetest.get_player_by_name(playerName)
+	if player ~= nil then
+		player:set_pos({x =325, y = 12, z = 1120})
 	end
 end
 
 minetest.register_chatcommand("spawn", {
-	privs = { spawn = true },
+	privs = {spawn = true},
 	func = movePlayerToSpawn
 })
 
 minetest.register_chatcommand("s", {
-	privs = { spawn = true },
+	privs = {spawn = true},
+	func = movePlayerToSpawn
+})
+
+minetest.register_chatcommand("spawn_old", {
+	privs = {spawn = true},
+	func = movePlayerToSpawn
+})
+
+minetest.register_chatcommand("s_o", {
+	privs = {spawn = true},
 	func = movePlayerToSpawn
 })
