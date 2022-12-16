455d325e492e35f2aa0b45b555836e014a9d6133
Niklp <89982526+Niklp09@users.noreply.github.com>
2022-12-16 14:23:40 +0100

    vote night, coloring

--- archtec_votes/vote_day_night.lua
+++ archtec_votes/vote_day_night.lua
@@ -1,7 +1,5 @@
 minetest.register_chatcommand("vote_day", {
-	privs = {
-		interact = true
-	},
+	privs = {interact = true},
 	func = function(name)
         vote.new_vote(name, {
             description = "Make day",
@@ -12,15 +10,49 @@ minetest.register_chatcommand("vote_day", {
 
             on_result = function(self, result, results)
                 if result == "yes" then
-                    minetest.chat_send_all("Vote passed, " .. #results.yes .. " to " .. #results.no .. ", Time will be set to day ")
-                    minetest.set_timeofday(0.5)
+                    minetest.chat_send_all("Vote passed, " .. #results.yes .. " to " .. #results.no .. ", Time will be set to day")
+                    minetest.set_timeofday(0.3)
                 else
-                    minetest.chat_send_all("Vote failed, " .. #results.yes .. " to " .. #results.no .. ", Time won't be set to day ")
+                    minetest.chat_send_all("Vote failed, " .. #results.yes .. " to " .. #results.no .. ", Time won't be set to day")
                 end
             end,
 
             on_vote = function(self, name, value)
-                minetest.chat_send_all(name .. " voted " .. value .. " to '" .. self.description .. "'")
+                if value == "yes" then
+                    minetest.chat_send_all(name .. " voted " .. minetest.colorize("#088A08", "YES") .. " to " .. self.description)
+                else
+                    minetest.chat_send_all(name .. " voted " .. minetest.colorize("#FF0000", "NO") .. " to " .. self.description)
+                end
+            end
+        })
+    end
+})
+
+minetest.register_chatcommand("vote_night", {
+	privs = {interact = true},
+	func = function(name)
+        vote.new_vote(name, {
+            description = "Make night",
+            help = "/yes,  /no  or  /abstain",
+            name = nil,
+            duration = 60,
+            perc_needed = 0.6,
+
+            on_result = function(self, result, results)
+                if result == "yes" then
+                    minetest.chat_send_all("Vote passed, " .. #results.yes .. " to " .. #results.no .. ", Time will be set to night")
+                    minetest.set_timeofday(0)
+                else
+                    minetest.chat_send_all("Vote failed, " .. #results.yes .. " to " .. #results.no .. ", Time won't be set to night")
+                end
+            end,
+
+            on_vote = function(self, name, value)
+                if value == "yes" then
+                    minetest.chat_send_all(name .. " voted " .. minetest.colorize("#088A08", "YES") .. " to " .. self.description)
+                else
+                    minetest.chat_send_all(name .. " voted " .. minetest.colorize("#FF0000", "NO") .. " to " .. self.description)
+                end
             end
         })
     end
