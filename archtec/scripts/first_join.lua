8c98c81c8cdcadced5d9476e5674183f0cac7fe0
Niklp <89982526+Niklp09@users.noreply.github.com>
2022-12-19 15:11:13 +0100

    fix formatting

--- archtec/scripts/first_join.lua
+++ archtec/scripts/first_join.lua
@@ -22,11 +22,11 @@ minetest.register_chatcommand("joined", {
 	func = function(player, param)
 		if minetest.get_player_by_name(param) then
 			return true,
-            C("#63d437", "First join of: ") .. C("#ffea00", param) .. "\n" ..
+			C("#63d437", "First join of: ") .. C("#ffea00", param) .. "\n" ..
 			C("#63d437", "Date: ") .. C("#ffea00", archtec.get_first_join_date(param))
-        elseif minetest.get_player_by_name(player) then
+		elseif minetest.get_player_by_name(player) then
 			return true,
-            C("#63d437", "First join of: ") .. C("#ffea00", player) .. "\n" ..
+			C("#63d437", "First join of: ") .. C("#ffea00", player) .. "\n" ..
 			C("#63d437", "Date: ") .. C("#ffea00", archtec.get_first_join_date(player))
 		else
 			return false, C("#ff0000", "This player isn't online")
