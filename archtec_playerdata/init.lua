664f025668cb4d33f4b2e59b3135cb133fe060a9
Niklp09 <89982526+Niklp09@users.noreply.github.com>
2023-01-16 17:46:40 +0100

    hacky value support (wip)

--- archtec_playerdata/init.lua
+++ archtec_playerdata/init.lua
@@ -1,7 +1,10 @@
---[[ Todo
--- value support
--- cache
+--[[ Doc:
+function archtec_playerdata.set(name, key, value)
+    - name is a playername
+    - key is an identifier
+    - value is the data (nil, number, string, boolean, table)
 ]]--
+
 archtec_playerdata = {}
 local datadir = minetest.get_worldpath() .. "/archtec_playerdata"
 assert(minetest.mkdir(datadir), "[archtec_playerdata] Could not create playerdata directory " .. datadir)
@@ -13,24 +16,37 @@ function archtec_playerdata.create(name)
     return true
 end
 
-function archtec_playerdata.get(name)
+function archtec_playerdata.get(name, key, default)
     local path = datadir .. "/" .. name .. ".txt"
     local file = io.open(path, "r")
+    local raw
     if file == nil then
         archtec_playerdata.create(name)
-        return false
     end
-    local raw = file:read()
     if file ~= nil then
+        raw = file:read()
         io.close(file)
     end
     local data = minetest.deserialize(raw)
-    return data
+    local value
+    if data == nil then
+        archtec_playerdata.set(name, key, default)
+        value = default
+    else
+        value = data[key]
+    end
+    return value
 end
 
-function archtec_playerdata.set(name, data)
+function archtec_playerdata.set(name, key, value)
     local path = datadir .. "/" .. name .. ".txt"
     local file = io.open(path, "w")
+    local raw = file:read()
+    local data = minetest.deserialize(raw)
+    if raw and data == nil then
+        data = {}
+    end
+    data[key] = value
     local new = minetest.serialize(data)
     file:write(new)
     io.close(file)
