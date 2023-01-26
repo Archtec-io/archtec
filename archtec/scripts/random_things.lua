2fb9d5e9d0687f79bb83d4e46f966df7b1b08516
Niklp09 <89982526+Niklp09@users.noreply.github.com>
2023-01-26 19:22:06 +0100

    fix crashes if dependencies are not available

--- archtec/scripts/random_things.lua
+++ archtec/scripts/random_things.lua
@@ -1,20 +1,26 @@
---remove building category
-unified_inventory.remove_category("building")
+-- remove building category
+if minetest.get_modpath("unified_inventory") then
+	unified_inventory.remove_category("building")
+end
 
-biome_lib.mapgen_elevation_limit = { ["min"] = 0, ["max"] = 48 }
+if minetest.get_modpath("biome_lib") then
+	biome_lib.mapgen_elevation_limit = { ["min"] = 0, ["max"] = 48 }
+end
 
-local S = minetest.get_translator("moreblocks")
-local def = minetest.registered_nodes["moreblocks:empty_shelf"]
-stairs.register_stair_and_slab(
-	"moreblocks:empty_shelf",
-	"moreblocks:empty_shelf",
-	def.groups,
-	def.tiles,
-	S("@1 Stair", def.description),
-	S("@1 Slab", def.description),
-	def.sounds,
-	true
-)
+if minetest.get_modpath("moreblocks") then
+	local S = minetest.get_translator("moreblocks")
+	local def = minetest.registered_nodes["moreblocks:empty_shelf"]
+	stairs.register_stair_and_slab(
+		"moreblocks:empty_shelf",
+		"moreblocks:empty_shelf",
+		def.groups,
+		def.tiles,
+		S("@1 Stair", def.description),
+		S("@1 Slab", def.description),
+		def.sounds,
+		true
+	)
+end
 
 if minetest.get_modpath("choppy") then
 	choppy.api.register_axe("default:enchanted_axe_bronze_durable")
