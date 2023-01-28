3b4442366273c86702ef724b556fa6d66dbac049
Niklp09 <89982526+Niklp09@users.noreply.github.com>
2023-01-28 19:07:05 +0100

    better logging

--- archtec_playerdata/init.lua
+++ archtec_playerdata/init.lua
@@ -5,9 +5,18 @@
 
 archtec_playerdata = {}
 local datadir = minetest.get_worldpath() .. "/archtec_playerdata"
-assert(minetest.mkdir(datadir), "[archtec_playerdata] Could not create playerdata directory " .. datadir)
+local cache = {}
+local fs_esc = minetest.formspec_escape
+local save_interval = 60
+local floor = math.floor
+local debug_mode = false
 
-cache = {} -- global for debug reasons
+minetest.register_on_mods_loaded(function()
+    if not minetest.mkdir(datadir) then
+		error("[archtec_playerdata] Failed to create datadir directory '" .. datadir .. "'!")
+	end
+    archtec_playerdata.save_all()
+end)
 
 -- struct: add new keys with default/fallback values! (Set always 0 as fallback!)
 local struct = {
@@ -22,18 +31,32 @@ local struct = {
 }
 
 -- helper funtions
-local function log(message)
+local function log_action(message)
+    if message ~= nil and message ~= "" then
+        minetest.log("action", "[archtec_playerdata] " .. message)
+    end
+end
+
+local function log_warning(message)
     if message ~= nil and message ~= "" then
         minetest.log("warning", "[archtec_playerdata] " .. message)
     end
 end
 
+local function log_debug(message)
+    if debug_mode then
+        if message ~= nil and message ~= "" then
+            minetest.log("warning", "[archtec_playerdata] " .. message)
+        end
+    end
+end
+
 local function valid_player(name)
     if name ~= nil and name ~= "" and type(name) == "string" then
-        -- log("valid_player: '" .. name .. "' is valid!")
+        log_debug("valid_player: '" .. name .. "' is valid!")
         return true
     else
-        log("valid_player: '" .. name .. "' is not valid!")
+        log_warning("valid_player: '" .. name .. "' is not valid!")
         return false
     end
 end
@@ -43,10 +66,10 @@ local function stats_file_exsist(name)
     local file = io.open(datadir .. "/" .. name .. ".txt", "r")
     if file ~= nil then
         file:close()
-        log("stats_file_exsist: file of '" .. name .. "' exsits")
+        log_debug("stats_file_exsist: file of '" .. name .. "' exsits")
         return true
     else
-        log("stats_file_exsist: file of '" .. name .. "' does not exsit")
+        log_debug("stats_file_exsist: file of '" .. name .. "' does not exsit")
         return false
     end
 end
@@ -64,20 +87,38 @@ local function is_valid(value)
     end
 end
 
-local function trim(s)
-    return s:match"^%s*(.*)":match"(.-)%s*$"
+local function divmod(a, b)
+    if type(a) ~= "number" or type(b) ~= "number" then
+        return
+    end
+    return floor(a / b), a % b
+end
+
+local function format_duration(seconds)
+	local display_hours, seconds_left = divmod(seconds, 3600)
+	local display_minutes, display_seconds = divmod(seconds_left, 60)
+	return ("%02d:%02d:%02d"):format(display_hours, display_minutes, display_seconds)
+end
+
+local function in_cache(name)
+    if not valid_player(name) then return false end
+    if cache[name] ~= nil then
+        return true
+    else
+        return false
+    end
 end
 
 -- (un)load/create data
 local function stats_create(name)
     if not valid_player(name) then return end
     if stats_file_exsist(name) then
-        log("stats_create: stats for '" .. name .. "' already exsists!")
+        log_warning("stats_create: stats for '" .. name .. "' already exsists!")
         return false
     end
     local file = io.open(datadir .. "/" .. name .. ".txt", "w")
     file:close()
-    log("stats_create: create stats file for '" .. name .. "'")
+    log_action("stats_create: create stats file for '" .. name .. "'")
     return true
 end
 
@@ -85,7 +126,7 @@ function archtec_playerdata.load(name)
     if not valid_player(name) then return end
     local file = io.open(datadir .. "/" .. name .. ".txt", "r")
     if not file then
-        log("load: file of '" .. name .. "' does not exsist!")
+        log_debug("load: file of '" .. name .. "' does not exsist!")
         stats_create(name)
         file = io.open(datadir .. "/" .. name .. ".txt", "r") -- try again
     end
@@ -93,23 +134,20 @@ function archtec_playerdata.load(name)
     file:close()
     local data
     if raw == nil then
-        log("load: file of '" .. name .. "' contains no data!")
+        log_warning("load: file of '" .. name .. "' contains no data!") -- ???
     else
         data = minetest.deserialize(raw)
         if data == nil then
             data = {} -- fix nil crashes due non-existing keys
         end
     end
-    -- print("raw: " .. raw)
-    -- print("data: " .. dump(data))
     for key, value in pairs(data) do
         if not (in_struct(key)) then
-            log("load: removing unknown key '" .. key .. "' of player '" .. name .. "'!")
+            log_warning("load: removing unknown key '" .. key .. "' of player '" .. name .. "'!")
             data[key] = nil -- remove unknown keys
         end
     end
     cache[name] = data
-    -- print(dump(cache))
 end
 
 function archtec_playerdata.unload(name)
@@ -122,55 +160,43 @@ function archtec_playerdata.load_offline(name) -- do not create/change any data 
     if not valid_player(name) then return end
     local file = io.open(datadir .. "/" .. name .. ".txt", "r")
     if not file then
-        log("load_offline: file of '" .. name .. "' does not exsist")
+        log_action("load_offline: file of '" .. name .. "' does not exsist")
         return
     end
     local raw = file:read("*a")
     file:close()
     local data
     if raw == nil then
-        log("load_offline: file of '" .. name .. "' contains no data!")
+        log_warning("load_offline: file of '" .. name .. "' contains no data!")
+        return
     else
         data = minetest.deserialize(raw)
     end
-    -- print("raw: " .. raw)
-    -- print("data: " .. dump(data))
     for key, value in pairs(data) do
         if not (in_struct(key)) then
-            log("load_offline: (temporary) removing unknown key '" .. key .. "' of player '" .. name .. "'!")
+            log_action("load_offline: (temporary) removing unknown key '" .. key .. "' of player '" .. name .. "'!")
             data[key] = nil -- remove unknown keys
         end
     end
     return data
 end
 
-function archtec_playerdata.in_cache(name)
-    if not valid_player(name) then return false end
-    if cache[name] ~= nil then
-        return true
-    else
-        return false
-    end
-end
-
 -- save data
 function archtec_playerdata.save(name)
     if not valid_player(name) then return end
     local data = cache[name]
-    -- save only if things changed (via table.concat)
     local file = io.open(datadir .. "/" .. name .. ".txt", "w")
     if not file then
-        log("save: file of '" .. name .. "' does not exsist!")
+        log_warning("save: file of '" .. name .. "' does not exsist!")
         return
     end
     local raw = minetest.serialize(data)
     if raw == nil then
-        log("save: raw data of '" .. name .. "' is nil!")
+        log_warning("save: raw data of '" .. name .. "' is nil!")
         return
     end
     file:write(raw)
     file:close()
-    log("save: saved data of '" .. name .. "'")
 end
 
 function archtec_playerdata.save_all()
@@ -181,12 +207,9 @@ function archtec_playerdata.save_all()
         archtec.playtimesave(name)
     end
     local after = minetest.get_us_time()
-    print("Took: " .. (after-before) / 1000 .. " ms")
-    minetest.after(20, archtec_playerdata.save_all)
-    -- print(dump(cache))
+    log_debug("Took: " .. (after-before) / 1000 .. " ms")
+    minetest.after(save_interval, archtec_playerdata.save_all)
 end
-
-minetest.after(6, archtec_playerdata.save_all)
 
 minetest.register_on_joinplayer(function(player)
     local name = player:get_player_name()
@@ -205,18 +228,18 @@ end)
 
 minetest.register_on_shutdown(function()
     archtec_playerdata.save_all()
-    log("shutdown: saved data!")
+    log_action("shutdown: saved all data!")
 end)
 
 -- get/set/mod data
 function archtec_playerdata.get(name, key)
-    if not valid_player(name) then return nil end
+    if not valid_player(name) then return end
     if type(key) ~= "string" then
-        log("get: key is not a string! '" .. key .. "'")
+        log_warning("get: key is not a string! '" .. key .. "'")
     end
     local val
     if cache[name] == nil then
-        log("get: cache for '" .. name .. "' is nil!")
+        log_debug("get: cache for '" .. name .. "' is nil!")
         return
     end
     if cache[name][key] == nil then
@@ -225,17 +248,17 @@ function archtec_playerdata.get(name, key)
         val = cache[name][key]
     end
     if val == nil then
-        log("get: key '" .. key .. "' is unknown!")
-        return nil
+        log_warning("get: key '" .. key .. "' is unknown!")
+        return
     end
     return val
 end
 
 function archtec_playerdata.set(name, key, value)
     if not valid_player(name) then return false end
-    if not is_valid(value) then return end
+    if not is_valid(value) then return false end
     if cache[name] == nil then
-        log("set: cache for '" .. name .. "' is nil!")
+        log_warning("set: cache for '" .. name .. "' is nil!")
         return false
     end
     cache[name][key] = value
@@ -245,12 +268,12 @@ end
 function archtec_playerdata.mod(name, key, value)
     if not valid_player(name) then return false end
     if type(value) ~= "number" then
-        log("mod: value '" .. value .. "' is not a number!")
+        log_warning("mod: value '" .. value .. "' is not a number!")
         return false
     end
     local old = archtec_playerdata.get(name, key)
     if old == nil then
-        log("mod: get returned nil for key '" .. key .. "' of '" .. name .. "'!")
+        log_warning("mod: get returned nil for key '" .. key .. "' of '" .. name .. "'!")
         return false
     end
     value = old + value
@@ -258,7 +281,7 @@ function archtec_playerdata.mod(name, key, value)
     return true
 end
 
--- test functions
+-- stats
 minetest.register_on_dignode(function(_, _, digger)
     local name = digger:get_player_name()
     if name ~= nil then
@@ -287,16 +310,8 @@ minetest.register_on_dieplayer(function(player, _)
     end
 end)
 
-local function divmod(a, b) return math.floor(a / b), a % b end
-
-local function format_duration(seconds)
-	local display_hours, seconds_left = divmod(seconds, 3600)
-	local display_minutes, display_seconds = divmod(seconds_left, 60)
-	return ("%02d:%02d:%02d"):format(display_hours, display_minutes, display_seconds)
-end
-
 local function stats(name, param) -- check for valid player doesn't work
-    local target = trim(param)
+    local target = param:trim()
     local data
     if target == "" or target == nil then
         target = name
@@ -305,7 +320,7 @@ local function stats(name, param) -- check for valid player doesn't work
         minetest.chat_send_player(name, "[stats]: Unknown player!")
         return
     end
-    if archtec_playerdata.in_cache(target) then
+    if in_cache(target) then
         data = table.copy(cache[target])
     else
         data = archtec_playerdata.load_offline(target)
@@ -329,21 +344,23 @@ local function stats(name, param) -- check for valid player doesn't work
     local formspec = {
         "formspec_version[4]",
         "size[5,5.5]",
-        "label[0.375,0.5;", minetest.formspec_escape("Stats of: " .. target), "]",
-        "label[0.375,1.0;", minetest.formspec_escape("Dug: " .. nodes_dug), "]",
-        "label[0.375,1.5;", minetest.formspec_escape("Placed: " .. nodes_placed), "]",
-        "label[0.375,2.0;", minetest.formspec_escape("Crafted: " .. crafted), "]",
-        "label[0.375,2.5;", minetest.formspec_escape("Died: " .. died), "]",
-        "label[0.375,3.0;", minetest.formspec_escape("Playtime: " .. playtime), "]",
-        "label[0.375,3.5;", minetest.formspec_escape("Average playtime: " .. avg_playtime), "]",
-        "label[0.375,4.0;", minetest.formspec_escape("Chatmessages: " .. chatmessages), "]",
-        "label[0.375,4.5;", minetest.formspec_escape("Join date: " .. joined), "]",
-        "label[0.375,5.0;", minetest.formspec_escape("Join count: " .. join_count), "]",
+        "label[0.375,0.5;", fs_esc("Stats of: " .. target), "]",
+        "label[0.375,1.0;", fs_esc("Dug: " .. nodes_dug), "]",
+        "label[0.375,1.5;", fs_esc("Placed: " .. nodes_placed), "]",
+        "label[0.375,2.0;", fs_esc("Crafted: " .. crafted), "]",
+        "label[0.375,2.5;", fs_esc("Died: " .. died), "]",
+        "label[0.375,3.0;", fs_esc("Playtime: " .. playtime), "]",
+        "label[0.375,3.5;", fs_esc("Average playtime: " .. avg_playtime), "]",
+        "label[0.375,4.0;", fs_esc("Chatmessages: " .. chatmessages), "]",
+        "label[0.375,4.5;", fs_esc("Join date: " .. joined), "]",
+        "label[0.375,5.0;", fs_esc("Join count: " .. join_count), "]",
     }
     minetest.show_formspec(name, "archtec_playerdata:stats", table.concat(formspec, ""))
 end
 
 minetest.register_chatcommand("stats", {
+    description = "Shows player stats",
+	privs = {interact = true},
     func = function(name, param)
         stats(name, param)
     end,
