21cc573f69fd76c51e7710dcbc0f78cf8ec84909
Niklp09 <89982526+Niklp09@users.noreply.github.com>
2023-02-11 12:01:18 +0100

    vote day/night changes

--- archtec/scripts/random_messages.lua
+++ archtec/scripts/random_messages.lua
@@ -2,7 +2,7 @@ local MESSAGE_INTERVAL = 600
 local S = minetest.get_translator("archtec")
 
 random_messages = {
-	"To check someone's playtime use '/playtime <player_name>'",
+	"To check someone's stats use '/stats <player_name>'",
 	"Visit our Discord server: https://discord.gg/txCMTMwBWm",
 	"Abusing '/vote_kick' will result in a ban for you",
 	"Read our rules at the spawn",
@@ -12,6 +12,8 @@ random_messages = {
 	"Use '/request_lava' if you want to place lava buckets",
 	"Use '/request_areas_high_limit' if you need more or bigger areas",
 	"You get the 'forceload' privilege automatically when you enter TA3",
+	"To start vote day/night you must have 3 etherium dust in your inventory",
+	"change your PvP state with the sword sysmbol in your inventory",
 }
 
 local function display_random_message(message)
@@ -21,10 +23,8 @@ local function display_random_message(message)
 	end
 end
 
-local random = math.random
-
 local function show_random_message()
-	local message = "[Info]: " .. random_messages[random(1, #random_messages)]
+	local message = "[Info]: " .. random_messages[math.random(1, #random_messages)]
 	display_random_message(message)
 end
 
@@ -33,5 +33,5 @@ local function step(dtime)
 	minetest.after(MESSAGE_INTERVAL, step)
 end
 
---start function
+-- start function
 minetest.after(MESSAGE_INTERVAL, step)
--- archtec_votes/vote_day_night.lua
+++ archtec_votes/vote_day_night.lua
@@ -1,6 +1,25 @@
+local function is_night()
+	return minetest.get_timeofday() < 0.2 or minetest.get_timeofday() > 0.75
+end
+
 minetest.register_chatcommand("vote_day", {
 	privs = {interact = true},
 	func = function(name)
+        if not is_night() then
+            minetest.chat_send_player(name, minetest.colorize("#FF0000", "You can't start a new vote, it's already day!"))
+            return
+        end
+
+        local player = minetest.get_player_by_name(name)
+        local inv = player:get_inventory()
+
+        if inv:contains_item("main", "ethereal:etherium_dust 3") then
+            inv:remove_item("main", "ethereal:etherium_dust 3")
+        else
+            minetest.chat_send_player(name, minetest.colorize("#FF0000", "To start a vote you must have 3 etherium dust in your inventory"))
+            return
+        end
+
         vote.new_vote(name, {
             description = "Make day",
             help = "/yes or /no",
@@ -31,6 +50,21 @@ minetest.register_chatcommand("vote_day", {
 minetest.register_chatcommand("vote_night", {
 	privs = {interact = true},
 	func = function(name)
+        if is_night() then
+            minetest.chat_send_player(name, minetest.colorize("#FF0000", "You can't start a new vote, it's already night!"))
+            return
+        end
+
+        local player = minetest.get_player_by_name(name)
+        local inv = player:get_inventory()
+
+        if inv:contains_item("main", "ethereal:etherium_dust 3") then
+            inv:remove_item("main", "ethereal:etherium_dust 3")
+        else
+            minetest.chat_send_player(name, minetest.colorize("#FF0000", "To start a vote you must have 3 etherium dust in your inventory"))
+            return
+        end
+
         vote.new_vote(name, {
             description = "Make night",
             help = "/yes or /no",
