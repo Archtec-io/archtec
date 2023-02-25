5d174fbbcb339b3a9289e7b58bf21c65bf21e2fa
Niklp09 <89982526+Niklp09@users.noreply.github.com>
2023-02-25 18:56:54 +0100

    alias feldweg nodes

--- archtec/scripts/aliases.lua
+++ archtec/scripts/aliases.lua
@@ -1,3 +1,11 @@
 minetest.register_alias("claycrafter:glass_of_water", "vessels:drinking_glass")
 minetest.register_alias("claycrafter:claycrafter", "default:stone")
 minetest.register_alias("claycrafter:claycrafter_active", "default:stone")
+
+minetest.register_alias("feldweg:feldweg", "cottages:feldweg")
+minetest.register_alias("feldweg:feldweg_crossing", "cottages:feldweg_crossing")
+minetest.register_alias("feldweg:feldweg_curve", "cottages:feldweg_curve")
+minetest.register_alias("feldweg:feldweg_end", "cottages:feldweg_end")
+minetest.register_alias("feldweg:feldweg_slope", "cottages:feldweg_slope")
+minetest.register_alias("feldweg:feldweg_slope_long", "cottages:feldweg_slope_long")
+minetest.register_alias("feldweg:feldweg_t_junction", "cottages:feldweg_t_junction")
