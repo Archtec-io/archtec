4577e330ed96c0bdf61e4d694df67ff6ba505ae9
Niklp09 <89982526+Niklp09@users.noreply.github.com>
2023-03-18 17:29:04 +0100

    improve skybox

--- archtec/scripts/skybox.lua
+++ archtec/scripts/skybox.lua
@@ -1,6 +1,14 @@
 local sky_start = tonumber(-100)
-local player_list = {}
 local timer = 0
+archtec.black_sky = {}
+
+local function allow_skybox_change(name)
+	local visible = auroras.player_data[name] and auroras.player_data[name].was_visible
+	if visible == true then
+		return false
+	end
+	return true
+end
 
 minetest.register_globalstep(function(dtime)
 	timer = timer + dtime
@@ -13,26 +21,26 @@ minetest.register_globalstep(function(dtime)
 	for _, player in pairs(minetest.get_connected_players()) do
 		local name = player:get_player_name()
 		local pos = player:get_pos()
-		local current = player_list[name] or ""
-
-		if pos.y > sky_start and current ~= "surface" then
-			player:set_sky({ type = "regular", clouds = true })
-			player:set_sun({ visible = true, sunrise_visible = true })
-			player:set_moon({ visible = true })
-			player:set_stars({ visible = true })
-			player_list[name] = "surface"
-		elseif pos.y < sky_start and current ~= "blackness" then
-			auroras.restore_sky(player)
-			player:set_sky({ base_color = "#000000", type = "plain", clouds = false })
-			player:set_sun({ visible = false, sunrise_visible = false })
-			player:set_moon({ visible = false })
-			player:set_stars({ visible = false })
-			player_list[name] = "blackness"
+		if allow_skybox_change(name) then
+			if pos.y > sky_start and archtec.black_sky ~= nil then
+				player:set_sky({ type = "regular", clouds = true })
+				player:set_sun({ visible = true, sunrise_visible = true })
+				player:set_moon({ visible = true })
+				player:set_stars({ visible = true })
+				archtec.black_sky[name] = nil
+			elseif pos.y < sky_start and archtec.black_sky ~= true then
+				auroras.restore_sky(player)
+				player:set_sky({ base_color = "#000000", type = "plain", clouds = false })
+				player:set_sun({ visible = false, sunrise_visible = false })
+				player:set_moon({ visible = false })
+				player:set_stars({ visible = false })
+				archtec.black_sky[name] = true
+			end
 		end
 	end
 end)
 
 minetest.register_on_leaveplayer(function(player)
 	local name = player:get_player_name()
-	player_list[name] = nil
+	archtec.black_sky[name] = nil
 end)
