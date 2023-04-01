f036c9cbefba7270951aa5132e13939992627e53
Niklp09 <89982526+Niklp09@users.noreply.github.com>
2023-04-01 11:45:51 +0200

    short some cheat log values more

--- archtec/scripts/cheat_log.lua
+++ archtec/scripts/cheat_log.lua
@@ -2,6 +2,12 @@ local function round(x)
     return x >= 0 and math.floor(x + 0.5) or math.ceil(x - 0.5)
 end
 
+-- https://stackoverflow.com/a/50082540
+local function short(number, decimals)
+    local power = 10^decimals
+    return math.floor(number * power) / power
+end
+
 local times = {}
 local pd_cache = {}
 
@@ -20,7 +26,7 @@ end
 local function handle_cheat(player, cheat)
     local name = player:get_player_name()
     local speed = player:get_velocity()
-    local lag = minetest.get_server_max_lag()
+    local lag = short(minetest.get_server_max_lag(), 2)
     local pos = player:get_pos()
     for i, x in pairs(speed) do
         speed[i] = round(x)
@@ -29,7 +35,7 @@ local function handle_cheat(player, cheat)
         pos[i] = round(x)
     end
     local info = get_pd(player, name)
-    notifyTeam("[archtec] Anticheat: player '" .. name .. "' ('" .. cheat.type .. "') speed: " .. tostring(speed) .. " pos: " .. tostring(pos) .. " lag: " .. lag .. " jitter: " .. info.avg_jitter .. " rtt: " .. info.avg_rtt)
+    notifyTeam("[archtec] Anticheat: player '" .. name .. "' ('" .. cheat.type .. "') speed: " .. tostring(speed) .. " pos: " .. tostring(pos) .. " lag: " .. lag .. " jitter: " .. short(info.avg_jitter, 7) .. " rtt: " .. short(info.avg_rtt, 5))
 end
 
 minetest.register_on_cheat(function(player, cheat)
