9c0f5233347ec6b33869f329788533469605b1e3
Niklp09 <89982526+Niklp09@users.noreply.github.com>
2023-05-02 20:43:44 +0200

    tool breakage sound support for tools with on_use

--- archtec/scripts/tool_break.lua
+++ archtec/scripts/tool_break.lua
@@ -31,7 +31,23 @@ minetest.register_on_mods_loaded(function()
             string.sub(tool.name, 1, 9) == "titanium:" or
             string.sub(tool.name, 1, 8) == "techage:"
         then
-            if not tool.after_use then
+            if tool.on_use then
+                local old_on_use = minetest.registered_tools[tool.name].on_use
+                minetest.override_item(tool.name, {
+                    on_use = function(itemstack, user, pointed_thing)
+                        local wdef = itemstack:get_definition()
+                        if itemstack:get_wear() > 60135 and wdef.sound and wdef.sound.breaks then
+                            local name = user:get_player_name()
+                            minetest.chat_send_player(name, minetest.colorize("#FF0000", "Your tool is about to break!"))
+                            minetest.sound_play(wdef.sound.breaks, {
+                                to_player = name,
+                                gain = 2.0,
+                            }, true)
+                        end
+                        return old_on_use(itemstack, user, pointed_thing)
+                    end
+                })
+            elseif not tool.after_use then
                 minetest.override_item(tool.name, {
                     after_use = break_warning
                 })
