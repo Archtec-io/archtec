7a76c33e7b2ed7a866a97148de54ae0f5cbe7592
Niklp09 <89982526+Niklp09@users.noreply.github.com>
2023-06-16 16:57:03 +0200

    add needed conditions to chainsaw help

--- archtec/scripts/chainsaw.lua
+++ archtec/scripts/chainsaw.lua
@@ -56,7 +56,7 @@ minetest.register_tool(":technic:chainsaw", {
 			if conditions(name) then
 				grant_priv(name, "archtec_chainsaw")
 			else
-				minetest.chat_send_player(name, minetest.colorize("#FF0000", "[chainsaw] You don't satisfy all conditions to use a chainsaw. See /stats"))
+				minetest.chat_send_player(name, minetest.colorize("#FF0000", "[chainsaw] You don't satisfy all conditions to use a chainsaw. Needed conditions: 20k nodes dug, 10k nodes placed, 24h playtime, 7 days or older account"))
 				return
 			end
 		end
