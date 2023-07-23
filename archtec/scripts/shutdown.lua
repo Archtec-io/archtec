271533059b3173ae4d4666370cf240bd6dc5db42
Niklp09 <89982526+Niklp09@users.noreply.github.com>
2023-07-23 21:54:53 +0200

    Shutdown fixes (trivial)

--- archtec/scripts/shutdown.lua
+++ archtec/scripts/shutdown.lua
@@ -4,11 +4,13 @@ minetest.register_chatcommand("sd", {
 	description = "Shuts the server down after a 10 seconds delay.",
 	privs = { server = true },
 	func = function(name, delay)
-		if delay == '' or type(tonumber(delay)) ~= "number" then delay = 10 end
-		local logStr = S("@1 requested a server shutdown in @2 seconds.", name, delay)
-		minetest.log("warning", logStr)
-		minetest.chat_send_all(minetest.colorize("#FF0", logStr))
-		discord.send(nil, ":anger: " .. logStr)
+		if delay == "" or type(tonumber(delay)) ~= "number" then delay = 10 end
+		-- notify all
+		local logmsg = name .. " requested a server shutdown in " .. delay .. " seconds."
+		minetest.log("warning", logmsg)
+		discord.send(nil, ":anger: " .. logmsg)
+		minetest.chat_send_all(minetest.colorize("#FF0", S("@1 requested a server shutdown in @2 seconds.", name, delay)))
+		-- shutdown, ask for reconnect
 		minetest.request_shutdown("The server is rebooting, please reconnect in about a minute.", true, tonumber(delay))
 	end
 })
