79028be87e1a9b2883b4835ff09b5f161e211936
Niklp09 <89982526+Niklp09@users.noreply.github.com>
2023-08-18 18:35:29 +0200

    test

--- archtec/scripts/common.lua
+++ archtec/scripts/common.lua
@@ -1,6 +1,6 @@
 function archtec.get_target(name, param)
 	local target = param:trim()
-	if target == "" or target == nil or type(target) ~= "string" then
+	if target == "" or type(target) ~= "string" then
 		target = name
 	end
 	return target
---
+++ archtec/spec/init_spec.lua
@@ -0,0 +1,46 @@
+require("mineunit")
+mineunit("core")
+mineunit("common/misc_helpers")
+
+fixture("archtec")
+
+local Sam = Player("Sam")
+
+function join()
+	mineunit:execute_on_joinplayer(Sam)
+end
+
+function leave()
+	mineunit:execute_on_leaveplayer(Sam)
+end
+
+-- Test common.lua
+describe("common.lua", function()
+	-- get_target()
+	it("get_target() 1", function()
+		assert.is_same("User2", archtec.get_target("User", "User2"))
+	end)
+
+	it("get_target() 2", function()
+		assert.is_same("User2", archtec.get_target("User", " User2 "))
+	end)
+
+	it("get_target() 3", function()
+		assert.is_same("User", archtec.get_target("User", ""))
+	end)
+
+	it("get_target() 4", function()
+		assert.is_same("User", archtec.get_target("User", {}))
+	end)
+
+	-- is_online()
+	join()
+	it("is_online() 1", function()
+		assert.is_true(archtec.is_online("Sam"))
+	end)
+
+	leave()
+	it("is_online() 2", function()
+		assert.is_false(archtec.is_online("Sam"))
+	end)
+end)
