4a8f687a9d02547832225ba3d5395b0ea32ee190
Niklp <nik@niklp.net>
2024-01-03 21:12:23 +0100

    Refactor idlekick

--- archtec/scripts/idlekick.lua
+++ archtec/scripts/idlekick.lua
@@ -1,46 +1,61 @@
 local S = archtec.S
-local timeout = archtec.time.hours(1) -- kick after 1h
+local timeout = archtec.time.hours(1) -- Kick after 1h
 local timer = 0
+local cache = {}
 
-local times, tag, ppos = {}, {}, {}
+local function now()
+	return minetest.get_us_time() / 1000000
+end
 
-local function now() return minetest.get_us_time() / 1000000 end
-local function bumpn(name) times[name] = now() return name end
+local function cache_init(name, pos)
+	cache[name] = {
+		last_active = now(),
+		nametag_edited = nil, -- Created later
+		pos = pos
+	}
+end
+
+
+local function bump_name(name)
+	cache[name].last_active = now()
+	return name
+end
 
 local function bump(player)
 	if not player then return end
 	local name = player:get_player_name()
-	return bumpn(name)
+
+	return bump_name(name)
 end
 
 local function get_nametag(name, player)
-	if not player then return end
 	local att = player:get_nametag_attributes()
 	if att.text == "" or att.text == nil then
-		att.text = name
+		return name
 	end
 	return att.text
 end
 
 minetest.register_on_joinplayer(function(player)
-	-- can happen when player was idle and got kicked
-	local hp = player:get_hp() or 20
+	local name = player:get_player_name()
+	-- Can happen when player was dead and got kicked
+	local hp = player:get_hp()
 	if hp == 0 then
-		local name = player:get_player_name()
 		minetest.log("action", "[archtec] Respawned dead player '" .. name .. "' on join")
 		minetest.chat_send_player(name, minetest.colorize("#00BD00", S("Server respawned you (you were dead without respawn option)")))
 		player:respawn()
 	end
-	-- create entrie
-	return bump(player)
+	-- Create data structure
+	cache_init(name, player:get_pos())
 end)
 
+-- Un-idle events
 minetest.register_on_placenode(function(_, _, player) bump(player) end)
-minetest.register_on_dignode(function(_, _, player) return bump(player) end)
-minetest.register_on_punchnode(function(_, _, player) return bump(player) end)
-minetest.register_on_chat_message(function(player) bumpn(player) end)
+minetest.register_on_dignode(function(_, _, player) bump(player) end)
+minetest.register_on_punchnode(function(_, _, player) bump(player) end)
 minetest.register_on_craft(function(_, player) bump(player) end)
-minetest.register_on_player_inventory_action(function(player) return bump(player) end)
+minetest.register_on_player_inventory_action(function(player) bump(player) end)
+minetest.register_on_chat_message(function(name) bump_name(name) end)
 
 minetest.register_globalstep(function(dtime)
 	timer = timer + dtime
@@ -49,29 +64,33 @@ minetest.register_globalstep(function(dtime)
 	end
 	timer = 0
 
+	local time = now()
+
 	for _, player in ipairs(minetest.get_connected_players()) do
 		local name = player:get_player_name()
 		local pos = player:get_pos()
-		local time = now()
 
-		if pos ~= ppos[name] then
-			bumpn(name)
-			ppos[name] = pos
+		if pos ~= cache[name].pos then
+			bump_name(name)
+			cache[name].pos = pos
 		end
 
-		if times[name] < time - timeout then
+		if cache[name].last_active < time - timeout then
 			minetest.kick_player(name, "Too long inactive")
 		end
-		if times[name] < time - 300 then
-			if not tag[name] then
-				tag[name] = true
+
+		if cache[name].last_active < time - 300 then
+			if not cache[name].nametag_edited then
+				cache[name].nametag_edited = true
+
 				local nametag = get_nametag(name, player) .. " (idle)"
 				player:set_nametag_attributes({
 					text = nametag
 				})
 			end
-		elseif tag[name] == true then
-			tag[name] = nil
+		elseif cache[name].nametag_edited then
+			cache[name].nametag_edited = nil
+
 			local nametag = get_nametag(name, player)
 			nametag = string.sub(nametag, 1, #nametag - 7)
 			player:set_nametag_attributes({
@@ -82,9 +101,6 @@ minetest.register_globalstep(function(dtime)
 end)
 
 minetest.register_on_leaveplayer(function(player)
-	if not player then return end
 	local name = player:get_player_name()
-	times[name] = nil
-	tag[name] = nil
-	ppos[name] = nil
+	cache[name] = nil
 end)
