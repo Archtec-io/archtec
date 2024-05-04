3b48b8c24f1797cd2891e22732794693fb9b00b1
Niklp <nik@niklp.net>
2024-05-04 11:19:35 +0200

    Fix ranks prioritization

--- archtec/scripts/ranks.lua
+++ archtec/scripts/ranks.lua
@@ -1,24 +1,22 @@
-local registered = {}
+local registered = {
+	admin = {prefix = "[Admin]", color = {a = 255, r = 230, g = 33, b = 23}, priv = "server",  prio = 3},
+	mod = {prefix = "[Mod]", color = {a = 255, r = 255, g = 83, b = 37}, priv = "staff", prio = 2},
+	builder = {prefix = "[Builder]", color = {a = 255, r = 0, g = 189, b = 0}, priv = "builder", prio = 1},
+}
 
 local function get_color(color)
 	return minetest.rgba(color.r, color.g, color.b, color.a)
 end
 
-local function register(name, def)
-	registered[name] = def
-end
-
-local function get_rank(name)
-	local privs = minetest.get_player_privs(name)
-	local rank
-	if privs.builder then
-		rank = "builder"
-	elseif privs.staff then
-		rank = "mod"
-	elseif privs.server then
-		rank = "admin"
+local function get_rank(player_name)
+	local privs = minetest.get_player_privs(player_name)
+	local rank = {prio = 0}
+	for name, def in pairs(registered) do
+		if privs[def.priv] and rank.prio < def.prio then
+			rank = {name = name, prio = def.prio}
+		end
 	end
-	return rank
+	return rank.name
 end
 
 local function update_nametag(player, remove)
@@ -47,24 +45,8 @@ end
 
 -- Assign/update rank on join player
 minetest.register_on_joinplayer(function(player)
-	if not player then return end
 	update_nametag(player, false)
 end)
-
-register("admin", {
-	prefix = "[Admin]",
-	color = {a = 255, r = 230, g = 33, b = 23},
-})
-
-register("mod", {
-	prefix = "[Mod]",
-	color = {a = 255, r = 255, g = 83, b = 37},
-})
-
-register("builder", {
-	prefix = "[Builder]",
-	color = {a = 255, r = 0, g = 189, b = 0},
-})
 
 minetest.register_chatcommand("ranks_reload", {
 	description = "Reload staff ranks",
