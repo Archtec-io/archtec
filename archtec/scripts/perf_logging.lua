edfa69b810dcdfd67d0762e2f87a419b887d8c71
Niklp <nik@niklp.net>
2025-08-29 17:04:11 +0200

    Perf logging: Adjust thresholds; tidy code

--- archtec/scripts/perf_logging.lua
+++ archtec/scripts/perf_logging.lua
@@ -1,40 +1,9 @@
 -- Slow ABM/NodeTimer/LBM logger
 local get_us_time, P2S = core.get_us_time, core.pos_to_string
 
-local abm_max_time = 5000 -- 5 ms
-local nt_max_time = 10000 -- 10 ms
-local lbm_max_time = 10000 -- 10 ms
-
-function archtec.abm_max_time(t)
-	abm_max_time = t
-end
-
-function archtec.nt_max_time(t)
-	nt_max_time = t
-end
-
-function archtec.lbm_max_time(t)
-	lbm_max_time = t
-end
-
-local function inc_abm(label, diff, pos, nn)
-	if diff > abm_max_time then
-		core.log("action", "[archtec] ABM '" .. label .. "', took '" .. diff .. "' us, pos '" .. P2S(pos) .. "', node '" .. nn .. "'")
-	end
-end
-
-local function inc_nt(diff, pos)
-	if diff > nt_max_time then
-		local nn = core.get_node(pos).name
-		core.log("action", "[archtec] NodeTimer took '" .. diff .. "' us, pos '" .. P2S(pos) .. "', node '" .. nn .. "'")
-	end
-end
-
-local function inc_lbm(label, diff, pos, nn)
-	if diff > lbm_max_time then
-		core.log("action", "[archtec] LBM '" .. label .. "', took '" .. diff .. "' us, pos '" .. P2S(pos) .. "', node '" .. nn .. "'")
-	end
-end
+local abm_max_time = 20000 -- 20 ms
+local nt_max_time = 20000 -- 20 ms
+local lbm_max_time = 20000 -- 20 ms
 
 core.register_on_mods_loaded(function()
 	for _, abm in ipairs(core.registered_abms) do
@@ -43,7 +12,10 @@ core.register_on_mods_loaded(function()
 			local t0 = get_us_time()
 			old_action(pos, node, active_object_count, active_object_count_wider)
 			local diff = get_us_time() - t0
-			inc_abm(abm.label or "??", diff, pos, node.name)
+
+			if diff > abm_max_time then
+				core.log("action", "[archtec] ABM '" .. abm.label or "??" .. "', took " .. diff .. " us, pos " .. P2S(pos) .. ", node '" .. node.name .. "'")
+			end
 		end
 	end
 
@@ -54,7 +26,12 @@ core.register_on_mods_loaded(function()
 				local t0 = get_us_time()
 				local res = old_action(pos, elapsed)
 				local diff = get_us_time() - t0
-				inc_nt(diff, pos)
+
+				if diff > nt_max_time then
+					local node = core.get_node(pos)
+					core.log("action", "[archtec] NodeTimer took " .. diff .. " us, pos " .. P2S(pos) .. ", node '" .. node.name .. "'")
+				end
+
 				return res
 			end
 		end
@@ -66,7 +43,10 @@ core.register_on_mods_loaded(function()
 			local t0 = get_us_time()
 			old_action(pos, node, dtime_s)
 			local diff = get_us_time() - t0
-			inc_lbm(lbm.label or "??", diff, pos, node.name)
+
+			if diff > lbm_max_time then
+				core.log("action", "[archtec] LBM '" .. lbm.label or "??" .. "', took " .. diff .. " us, pos " .. P2S(pos) .. ", node '" .. node.name .. "'")
+			end
 		end
 	end
 end)
